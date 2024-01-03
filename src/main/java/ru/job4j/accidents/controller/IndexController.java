39e6bf22b2bf0b550c99764ab99a9a0fa675d2ae
PavelChupin <pavelchupin94@yandex.ru>
2024-01-03 16:25:31 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -2,13 +2,15 @@ package ru.job4j.accidents.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Pavel Chupin");
         return "index";
     }
 
