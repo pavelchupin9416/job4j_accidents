834231f33e941e5edb16ad1694c91ee06ff62a34
pavel.chupin <pavelchupin94@yandex.ru>
2024-01-29 18:08:56 +0300

    0. Spring DataSource [#6878]

--- src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
@@ -1,23 +1,31 @@
 package ru.job4j.accidents.repository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-import java.util.List;
+
+import java.util.Collection;
+import java.util.Optional;
+
 
 @Repository
 @AllArgsConstructor
-public class AccidentJdbcTemplate {
+public class AccidentJdbcTemplate implements AccidentRepository {
     private final JdbcTemplate jdbc;
 
+    @Override
     public Accident save(Accident accident) {
         jdbc.update("insert into accidents (name) values (?)",
+                accident.getName());
+        jdbc.update("insert into accident_rules (name) values (?)",
                 accident.getName());
         return accident;
     }
 
-    public List<Accident> getAll() {
+    @Override
+    public Collection<Accident> findAll() {
         return jdbc.query("select id, name from accidents",
                 (rs, row) -> {
                     Accident accident = new Accident();
@@ -26,4 +34,35 @@ public class AccidentJdbcTemplate {
                     return accident;
                 });
     }
+
+    @Override
+    public boolean deleteById(int id) {
+        return true;
+    }
+
+
+    @Override
+    public boolean update(Accident accident) {
+        return true;
+    }
+
+    @Override
+    public Optional<Accident> findById(int id) {
+        return Optional.empty();
+    }
+    /*
+
+
+
+
+    @Override
+    public boolean update(Accident accident) {
+        return accidents.computeIfPresent(accident.getId(), (id, oldAccident) -> new Accident(oldAccident.getId(),
+                accident.getName(), accident.getText(), accident.getAddress(), accident.getType(), accident.getRules())) != null;
+    }
+
+    @Override
+    public Optional<Accident> findById(int id) {
+        return Optional.ofNullable(accidents.get(id));
+    }*/
 }
